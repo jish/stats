a34e553118a50e678151e725caf94a625b00fe94
Josh Lubaway <josh.lubaway@gmail.com>
2018-04-30 21:50:59 -0700

    Property based testing
    In addition to the doc tests on top of each function, we also test
    some properties of each function to help us catch edge cases.

--- lib/stats.ex
+++ lib/stats.ex
@@ -1,18 +1,44 @@
 defmodule Stats do
   @moduledoc """
-  Documentation for Stats.
+  Calculate some stats on the list of Integers provided
   """
 
   @doc """
-  Hello world.
+  Add up all of the Integers in the list and return the sum.
 
   ## Examples
 
-      iex> Stats.hello
-      :world
+      iex> Stats.sum([1, 2, 3])
+      6
 
   """
-  def hello do
-    :world
+  def sum(list) do
+    Enum.reduce(list, 0, fn (x, y) -> x + y end)
+  end
+
+  @doc """
+  Count how many items are in the list
+
+  ## Examples
+
+      iex> Stats.count([1, 2, 3])
+      3
+
+  """
+  def count(list) do
+    length(list)
+  end
+
+  @doc """
+  Find the median value in the list
+
+  ## Examples
+
+      iex> Stats.average([1, 2, 3])
+      2.0
+
+  """
+  def average(list) do
+    sum(list) / count(list)
   end
 end
--- test/stats_test.exs
+++ test/stats_test.exs
@@ -1,8 +1,26 @@
 defmodule StatsTest do
   use ExUnit.Case
+  use ExCheck
+
   doctest Stats
 
-  test "greets the world" do
-    assert Stats.hello() == :world
+  describe "lists of integers" do
+    property "single item lists are their own sum" do
+      for_all number in real() do
+        assert Stats.sum([number]) == number
+      end
+    end
+
+    property "count is a positive integer" do
+      for_all list in list(int()) do
+        assert Stats.count(list) >= 0
+      end
+    end
+
+    property "sum equals average times count" do
+      for_all l in such_that(l in list(int()) when length(l) > 0) do
+        abs(Stats.sum(l) - Stats.count(l) * Stats.average(l)) < 1.0e-6
+      end
+    end
   end
 end
